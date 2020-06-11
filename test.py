b60344eaae80d686e26c4cbc73a4b164d9d5d91c
Marco Gellecanao <marcogelle@berkeley.edu>
2020-06-10 18:02:30 -0700

    testing more

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
 from tkinter import *
 
 window = Tk()
-label = Label(window, text="Hello World!")
+
+label = Label(window, text="Hello World!", fg="white", bg="black")
 label.pack()
+
+button = Button(
+    text="click me?",
+    width=25,
+    height=5,
+    bg="black",
+    fg="white"
+)
+button.pack()
+
+entry = tk.Entry(fg="white", bg="black", width=50)
+
 window.mainloop()
