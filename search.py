5278d2ef37c57b9eab127dd40aa639128b4476ad
Marco Gellecanao <marcogelle@berkeley.edu>
2020-06-13 13:37:54 -0700

    working on A*

--- search.py
+++ search.py
@@ -14,14 +14,24 @@ class AStar(AbstractSearch):
 
     def search(self, start: Node, dest: Node):
         open = PriorityQueue()
-        g = {start.pos_str(): 0}
         open.put((0, start))
         closed = ()
+        g = {start.pos_str(): 0}
+        parents = {start.pos_str(): None}
         while open:
             curr = open.get()
             closed.add(curr)
-            for s in curr.get_succ():
+            for next in curr.get_succ():
                 cost = g[curr.pos_str()] + 1
-                if s in [item[1] for item in open.queue]: # check if s in open
-                    pass
-                    # TODO: finish implementing this
+                if next in closed:
+                    continue
+                if next not in [x[1] for x in open.queue]:
+                    parents[next.pos_str()] = curr
+                    g[next.pos_str()] = cost
+                    f_next = cost + self.heur(next, dest)
+                    open.put((f_next, next))
+                elif cost < g[start.pos_str()]:
+                    #TODO: remove from list
+
+    def heur(self, node: Node, dest: Node) -> int:
+        pass
