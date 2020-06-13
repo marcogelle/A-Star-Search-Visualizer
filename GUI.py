4c0b65e6de7c1432acf68452747f578be2c48022
Marco Gellecanao <marcogelle@berkeley.edu>
2020-06-12 17:05:09 -0700

    search file created

--- GUI.py
+++ GUI.py
@@ -18,19 +18,19 @@ class GUI:
         self.initial_click = None
         self.initial_black = False
 
-        self.__draw_top_bar()
-        self.__draw_grid()
+        self.draw_top_bar()
+        self.draw_grid()
 
-    def __draw_top_bar(self):
+    def draw_top_bar(self):
         """Sets up all widgets for the top bar of the UI."""
         frm_input = tk.Frame(master=self.root)
-        self.__draw_start_input(frm_input)
-        self.__draw_destination_input(frm_input)
-        self.__draw_start_buttons(frm_input)
-        self.__draw_position_tracker(frm_input)
+        self.draw_start_input(frm_input)
+        self.draw_destination_input(frm_input)
+        self.draw_start_buttons(frm_input)
+        self.draw_position_tracker(frm_input)
         frm_input.pack()
 
-    def __draw_grid(self):
+    def draw_grid(self):
         """Creates the grid."""
         frm_grid = tk.Frame(master=self.root)
         for r in range(NUM_ROWS):
@@ -43,16 +43,16 @@ class GUI:
                     height=SPOT_SIZE
                 )
                 frm.grid(row=r, column=c)
-                frm.bind("<Button-1>", self.__handle_click)
-                frm.bind("<B1-Motion>", self.__handle_drag)
-                frm.bind("<Enter>", lambda event: self.__track_position(event))
+                frm.bind("<Button-1>", self.handle_click)
+                frm.bind("<B1-Motion>", self.handle_drag)
+                frm.bind("<Enter>", lambda event: self.track_position(event))
                 self.node_map.add(Node(frm, c, NUM_ROWS-r-1, self.node_map))
         frm_grid.pack()
 
-    def __draw_start_input(self, frame):
+    def draw_start_input(self, frame):
         """Sets up widgets for inputting the starting position."""
         def draw_start():
-            x, y = ent_start_x.get(), ent_start_y.get()
+            x, y = int(ent_start_x.get()), int(ent_start_y.get())
             node = self.node_map.get_from_pos(x, y)
             node.get_frm()["bg"] = "red"
             if self.start_node:
@@ -72,15 +72,16 @@ class GUI:
         lbl_start_y.pack(side=tk.LEFT)
         ent_start_y.pack(side=tk.LEFT)
 
-    def __draw_destination_input(self, frame):
+    def draw_destination_input(self, frame):
         """Sets up widgets for inputting the destination position."""
         def draw_dest():
-            x, y = ent_dest_x.get(), ent_dest_y.get()
+            x, y = int(ent_dest_x.get()), int(ent_dest_y.get())
             node = self.node_map.get_from_pos(x, y)
             node.get_frm()["bg"] = "green"
             if self.dest_node:
                 self.dest_node.get_frm()["bg"] = "#d9d9d9"
             self.dest_node = node
+
         lbl_dest = tk.Button(master=frame, font="Helvetica 11 bold",
             text="Destination ", command=draw_dest)
         lbl_dest_x = tk.Label(master=frame, text="X:")
@@ -94,13 +95,13 @@ class GUI:
         lbl_dest_y.pack(side=tk.LEFT)
         ent_dest_y.pack(side=tk.LEFT)
 
-    def __draw_start_buttons(self, frame):
+    def draw_start_buttons(self, frame):
         """Creates a button for starting the A* search."""
         btn_Astar = tk.Button(master=frame, text="Start A* Search",
             bg="#2f4454", fg="white")
         btn_Astar.pack(side=tk.LEFT, padx=(20,0))
 
-    def __draw_position_tracker(self, frame):
+    def draw_position_tracker(self, frame):
         """Creates the labels that track the cursor coordinates."""
         lbl_x = tk.Label(master=frame, text="x:")
         self.lbl_mouse_x = tk.Label(master=frame, width=2, text="?")
@@ -112,7 +113,7 @@ class GUI:
         lbl_y.pack(side=tk.LEFT, padx=(10,0))
         self.lbl_mouse_y.pack(side=tk.LEFT)
 
-    def __handle_click(self, event):
+    def handle_click(self, event):
         """Event handler that changes the  color of the spot that is clicked."""
         self.initial_click = event.widget
         if self.initial_click["bg"] == "#d9d9d9":
@@ -122,7 +123,7 @@ class GUI:
             self.initial_click["bg"] = "#d9d9d9"
             self.initial_black = False
 
-    def __handle_drag(self, event):
+    def handle_drag(self, event):
         """Event handler that changes the color of the spot that
         is dragged over."""
         widget = event.widget.winfo_containing(event.x_root, event.y_root)
@@ -136,7 +137,7 @@ class GUI:
                 else:
                     self.current_widget["bg"] = "#d9d9d9"
 
-    def __track_position(self, event):
+    def track_position(self, event):
         """Event handler that updates the current grid position in
         x and y coordinates."""
         widget = event.widget
---
+++ search.py
@@ -0,0 +1,9 @@
+from node import *
+
+class Search:
+    def __init__(self):
+        pass
+
+class AStar:
+    def __init__(self):
+        pass
