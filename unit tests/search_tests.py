4d78d86465d4dc0491ab0ca7739d85e8182a4c9c
Marco Gellecanao <marcogelle@berkeley.edu>
2020-07-19 00:22:22 -0700

    updated search unit test

--- unit tests/search_tests.py
+++ unit tests/search_tests.py
@@ -5,6 +5,7 @@ import tkinter as tk
 sys.path.insert(1, '../search visualizer')
 from node import *
 from search import *
+from heuristics import *
 
 class TestNode(unittest.TestCase):
     def __init__(self, *args, **kwargs):
@@ -20,40 +21,45 @@ class TestNode(unittest.TestCase):
         walls = set()
         start = self.map.get_from_pos(1, 1)
         dest = self.map.get_from_pos(5, 4)
-        a_star = AStar(walls, Heuristics.manhattan)
+        a_star = AStar(manhattan)
         path = a_star.search(start, dest)
         self.print_test('test_trivial', start, dest, walls, path)
 
     def test_wall_simple(self):
         walls = set()
         for x, y in [[3, 1], [3, 2], [3, 3], [3, 4]]:
-            walls.add(self.map.get_from_pos(x, y))
+            node = self.map.get_from_pos(x, y)
+            walls.add(node)
+            node.wall_on()
         start = self.map.get_from_pos(1, 1)
         dest = self.map.get_from_pos(5, 4)
-        a_star = AStar(walls, Heuristics.manhattan)
+        a_star = AStar(manhattan)
         path = a_star.search(start, dest)
         self.print_test('test_wall_simple', start, dest, walls, path)
 
     def test_no_path(self):
-        walls= set()
+        walls = set()
         for x, y in [[0, 2], [1, 2], [2, 2], [3, 2], [3, 1], [3, 0]]:
-            walls.add(self.map.get_from_pos(x, y))
+            node = self.map.get_from_pos(x, y)
+            walls.add(node)
+            node.wall_on()
         start = self.map.get_from_pos(1, 1)
         dest = self.map.get_from_pos(5, 4)
-        a_star = AStar(walls, Heuristics.manhattan)
+        a_star = AStar(manhattan)
         path = a_star.search(start, dest)
         self.print_test('test_no_path', start, dest, walls)
         self.assertEqual(len(path), 0)
 
     def print_test(self, name, start, dest, walls, path=None):
+        print()
         print(name)
-        print(f"Start: {start.pos_str()}, Destination: {dest.pos_str()}")
+        print(f"Start: {start}, Destination: {dest}")
         if walls:
-            print(f"Walls: { {w.pos_str() for w in walls} }")
+            print(f"Walls: { {w for w in walls} }")
         else:
             print('Walls: None')
         if path is not None:
-            print(f"path: { [node.pos_str() for node in path] }")
+            print(f"path: { [node for node in path] }")
         print()
 
 if __name__ == '__main__':
